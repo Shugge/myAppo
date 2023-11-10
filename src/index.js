22908b770af636c98292d39ff14fa3d312279110
Shugge <deandersson22@gmail.com>
2023-11-10 18:13:29 +0100

    func changez

--- src/index.js
+++ src/index.js
@@ -20,26 +20,34 @@ const books = [
 ];
 
 const BookList = () => {
+    const displayValue = () => {
+        console.log('oi');
+    }
+    const getBook = (bookId) => {
+        const book = books.find((item) => item.id == bookId);
+        console.log(book);
+    }
     return (        
         <section className="booklist">
             {
                 books.map((book) => {
                     return (
-                        <Book {...book} key={book.id} />
+                        <Book {...book} key={book.id} getBook={getBook} />
                     );
                 })
             }
         </section>
     );
-}
+};
 
 const Book = (props) => {
-    console.log(props);
-    const {img, title, author} = props;
+    // console.log(props);
+    const {img, title, author, getBook, id} = props;
     return (
         <article className="book">
             <img src={img} alt={title}/>
             <h2>{title}</h2>
+            <button onClick={() => getBook(id)}>klick</button>
             <h4>{author}</h4>
         </article>
     );
